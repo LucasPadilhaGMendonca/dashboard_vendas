8109e463e67edc4219480a874c4e03ff1663c8a6
LucasPadilhaGMendonca <ti.lucaspmendonca@gmail.com>
2025-06-18 19:30:47 -0300

    Update Dashboard.py

--- Dashboard.py
+++ Dashboard.py
@@ -12,7 +12,7 @@ def formata_numero(valor, prefixo = ''):
         valor /= 1000
     return f'{prefixo} {valor:.2f} milhões'
 
-st.title('DASHBOARD DE VENDAS :shopping_trolley')
+st.title("DASHBOARD DE VENDAS 🛒")
 
 url = 'https://labdados.com/produtos'
 regioes = ['Brasil', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul']
